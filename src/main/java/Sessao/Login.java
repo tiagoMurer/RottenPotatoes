e7a51d34a663268d79639e86a5e9983475dc417f
tiagoMurer <tiagostaurenghi@hotmail.com>
2019-06-21 20:13:10 -0300

    correcao

--- src/main/java/Sessao/Login.java
+++ src/main/java/Sessao/Login.java
@@ -1,13 +1,32 @@
 package Sessao;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Login {
-    String id_field;
-    String pswd_field;
 
-    Login(){
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField inputid;
+
+    @FXML
+    private TextField inputpswd;
+
+    @FXML
+    private Button loginbtn;
+    
+    @FXML
+    void initialize() {
+        assert inputid != null : "fx:id=\"inputemail\" was not injected: check your FXML file 'Untitled'.";
+        assert inputpswd != null : "fx:id=\"inputpswd\" was not injected: check your FXML file 'Untitled'.";
+        assert loginbtn != null : "fx:id=\"loginbtn\" was not injected: check your FXML file 'Untitled'.";
 
     }
-
-    public void signin(String id, String pswd){
-    }
-}
